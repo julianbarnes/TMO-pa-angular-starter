fc9d3baff0f6d45b056d7ff838403d4d8f376041
Julian <julian.barnes100@gmail.com>
2021-12-01 10:47:17 -0500

    Adds reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pa-starter-angular';
+  recipeForm: FormGroup;
+  recipeList: {
+    recipeName: string,
+    recipeInstructions: string
+  }[];
+  
+  constructor() { 
+    this.recipeList = [];
+    this.recipeForm = new FormGroup({
+      recipeNameControl: new FormControl(""),
+      recipeInstructionsControl: new FormControl("")
+    })
+  }
+  showRecipeForm: boolean = false;
+
+  /**
+   * @description adds a recipe
+   */
+   public addRecipe() {
+    // show the button again 
+    this.showRecipeForm = false;
+    // Add to list
+    this.recipeList.push({
+      recipeName: this.recipeForm.get('recipeNameControl')?.value,
+      recipeInstructions: this.recipeForm.get('recipeInstructionControl')?.value
+      }
+    )
+    // Reset the form
+    this.recipeForm = this.createFormGroup();
+  }
+
+  private createFormGroup() {
+    return new FormGroup({
+      recipeNameControl: new FormControl(""),
+      recipeInstructionsControl: new FormControl("")
+    });
+  }
+
+  /**
+   * @description shows the recipe form  
+   */
+   public toggleShowRecipeForm() {
+    this.showRecipeForm = true;
+  }
+
 }
